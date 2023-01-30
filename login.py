94f98bf397e85c9ec54b2112fe2907057cdaf241
AFZAL <110601700+AFAZL@users.noreply.github.com>
2023-01-30 20:39:23 +0530

    login page

---
+++ login.py
@@ -0,0 +1,46 @@
+from tkinter import *
+import sqlite3
+root = Tk()
+root.title("Python: Simple Login Application")
+width = 400
+height = 280
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+#==============================VARIABLE======================================
+USERNAME = StringVar()
+PASSWORD = StringVar()
+ 
+#==============================FRAME=========================================
+Top = Frame(root, bd=2,  relief=RIDGE)
+Top.pack(side=TOP, fill=X)
+Form = Frame(root, height=200)
+Form.pack(side=TOP, pady=20)
+ 
+#==============================LABEL=========================================
+lbl_title = Label(Top, text = "Python: Simple Login Application", font=('arial', 15))
+lbl_title.pack(fill=X)
+lbl_username = Label(Form, text = "Username:", font=('arial', 14), bd=15)
+lbl_username.grid(row=0, sticky="e")
+lbl_password = Label(Form, text = "Password:", font=('arial', 14), bd=15)
+lbl_password.grid(row=1, sticky="e")
+lbl_text = Label(Form)
+lbl_text.grid(row=2, columnspan=2)
+ 
+#==============================ENTRY WIDGET==================================
+username = Entry(Form, textvariable=USERNAME, font=(14))
+username.grid(row=0, column=1)
+password = Entry(Form, textvariable=PASSWORD, show="*", font=(14))
+password.grid(row=1, column=1)
+#==============================INITIALIATIO==================================
+if __name__ == '__main__':
+    root.mainloop()
+
+ 
+#==============================BUTTON WIDGETS=================================
+btn_login = Button(Form, text="Login", width=45, command=Login)
+btn_login.grid(pady=25, row=3, columnspan=2)
+btn_login.bind('<Return>', Login)
